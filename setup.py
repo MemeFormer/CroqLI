b0f5b590f046761836b215b1f3c0e2446c591690
meme_f4rmer <130863507+MemeFormer@users.noreply.github.com>
2024-06-28 18:56:00 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# croqli/setup.py 
+
+from setuptools import setup, find_packages
+
+setup(
+    name="croqli",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
